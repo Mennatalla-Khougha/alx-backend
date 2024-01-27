4433bd828383e036f1cdbf99edab5209b471d6d1
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-27 13:40:31 +0200

    task 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,1 +1,77 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """The goal here is that if between two queries, certain rows are
+        removed from the dataset, the user does not miss items from dataset
+        when changing page.
+
+        Args:
+            index (int, optional): Defaults to None.
+            page_size (int, optional): Defaults to 10.
+
+        Returns:
+            Dict:
+        """
+        assert (index >= 0 and isinstance(index, int)) or index is None
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.__dataset
+        total = len(dataset)
+        index = 0 if index is None else index
+        assert 0 <= index < total
+
+        # calculate the last index in the current page
+        end = min(index + page_size, total)
+
+        # get the data for the current page
+        data = dataset[index: end]
+
+        # calculate the next index for the next page
+        next_index = end if end < total else None
+
+        result = {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
+
+        return result
