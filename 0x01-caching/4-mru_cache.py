888e8d0ac1ad2a0ecd2e749728485bca23d498f2
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-30 18:34:04 +0200

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""class LRUCaching that inherits from BaseCaching"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCaching defines:
+      - put and get method to retrieve data
+    """
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed_key, _ = self.cache_data.popitem(last=False)
+                print(f'DISCARD: {removed_key}')
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+            return self.cache_data[key]
+        return None
