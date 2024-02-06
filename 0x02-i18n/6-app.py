9873a76eeb3c64a1142934551203eaa907a6d17f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-06 19:14:46 +0200

    task 6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,1 +1,63 @@
 #!/usr/bin/env python3
+"""Instantiate the Babel object"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """Set Babel’s default locale"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Function that returns a user dictionary or None"""
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """Use get_user to find a user if any, and set it as a global"""
+    g.user = get_user()
+    # if g.user:
+    #     locale = g.user['locale'] or app.config['BABEL_DEFAULT_LOCALE']
+    #     babel.locale_selector_func = lambda: locale
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    locale = request.args.get("locale")
+    if locale:
+        return locale if locale in app.config['LANGUAGES'] else None
+    if g.user and 'locale' in g.user:
+        return g.user['locale'] if g.user['locale'] in\
+            app.config['LANGUAGES'] else None
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """The base route"""
+    return render_template('6-index.html', user=g.user)
+
+
+if __name__ == '__main__':
+    app.run()
