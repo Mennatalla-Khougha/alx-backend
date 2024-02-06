b9594a32bb07dcb74c9c827cb7891c73a42c6409
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-06 13:30:39 +0200

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""Instantiate the Babel object"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Set Babel’s default locale"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    locale = request.args.get("locale")
+    supported_locales = app.config['LANGUAGES']
+    if locale in supported_locales:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """The base route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
