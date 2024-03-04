d9d10f3a3bfa78c7ea488720fb27147638584be7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-04 14:31:16 +0200

    task 0: 1

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,10 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    host: '127.0.0.1',
-    port: 6379
-})
-    
-client.on('error', err => console.log('Redis client not connected to the server:', err))
-    
-client.on('ready', () => console.log('Redis client connected to the server'))
+  host: '127.0.0.1',
+  port: 6379
+});
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.on('ready', () => console.log('Redis client connected to the server'));
--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+import { createClient, print } from 'redis';
+
+const client = createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      throw new Error();
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
