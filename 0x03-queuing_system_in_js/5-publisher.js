338099beac107a94d620df141a20dd223281c43e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-04 20:28:53 +0200

    task 5

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+import { createClient } from 'redis';
+
+const client = createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+function publishMessage (msg, time) {
+  setTimeout(() => {
+    console.log(`About to send ${msg}`);
+    client.publish('holberton school channel', msg);
+  }, time);
+}
+
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+import { createClient } from 'redis';
+
+const client = createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+client.subscribe('holberton school channel');
+
+client.on('message', (ch, msg) => {
+  console.log(msg);
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
