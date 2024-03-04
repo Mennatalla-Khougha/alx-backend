a889278d7a39f7689a637e27a7d207bf13a6c79e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-04 10:04:31 +0200

    task 1

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+import { createClient } from 'redis';
+
+const client = createClient({
+    host: '127.0.0.1',
+    port: 6379
+})
+    
+client.on('error', err => console.log('Redis client not connected to the server:', err))
+    
+client.on('ready', () => console.log('Redis client connected to the server'))
