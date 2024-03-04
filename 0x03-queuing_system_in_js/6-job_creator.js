a418fffc8794b4d394d3871c811cded0a37f31ba
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-04 21:18:01 +0200

    task 6

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import { createQueue } from "kue";
+
+const queue = createQueue();
+
+const data = {
+	phoneNumber: "01231456789",
+	message: "This is test msg",
+};
+
+const job = queue.create("push_notification_code", data).save((err) => {
+	if (!err) {
+		console.log(`Notification job created: ${job.id}`);
+	}
+});
+
+job.on("failed", () => console.log("Notification job failed"));
+job.on("complete", () => console.log("Notification job completed"));
