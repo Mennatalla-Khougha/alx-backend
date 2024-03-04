6bbc52199a88bb8d5f245ba83f48aed21081f9d9
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-04 22:55:09 +0200

    task 7

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import { createQueue } from "kue";
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, msg) {
+	console.log(`Sending notification to ${phoneNumber}, with message: ${msg}`);
+}
+
+queue.process("push_notification_code", (job, done) => {
+	sendNotification(job.data.phoneNumber, job.data.message);
+	done();
+});
