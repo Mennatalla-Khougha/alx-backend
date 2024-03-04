a97f71c78ab1fb66bf52242b2e0b0348ce51f5ed
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-05 01:02:10 +0200

    task 10

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+function createPushNotificationsJobs(jobs, queue) {
+	if (!Array.isArray(jobs)) {
+		throw new Error("Jobs is not an array");
+	}
+	jobs.forEach((ele) => {
+		const job = queue.create("push_notification_code_3", ele).save((err) => {
+			if (!err) {
+				console.log(`Notification job created ${job.id}`);
+			}
+		});
+		job.on("complete", () =>
+			console.log(`Notification job ${job.id} completed`)
+		);
+		job.on("failed", (err) =>
+			console.log(`Notification job ${job.id} failed: ${err}`)
+		);
+		job.on("progress", (progress, data) =>
+			console.log(`Notification job ${job.id} ${progress}% complete`)
+		);
+	});
+}
+
+module.exports = createPushNotificationsJobs;
